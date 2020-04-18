70fd87444e864400af00b08eda54308628d14589
Thierry CANTENOT <thierrycantenot@gmail.com>
2020-04-18 14:11:57 +0200

    Adds shading for disco ball

---
+++ LD46-DiscoViking/ld-46/Assets/Scripts/UI/DiscoBallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoBallScript : MonoBehaviour
+{
+    public float rotateSpeed = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+}
