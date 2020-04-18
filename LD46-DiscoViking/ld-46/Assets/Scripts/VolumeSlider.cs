28f8688cea8b0679d24766b10cc1e22bfc46eca6
Otakode <marchal.cl@gmail.com>
2020-04-19 00:21:32 +0200

    volume and fix DiscoBall

---
+++ LD46-DiscoViking/ld-46/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSlider : MonoBehaviour
+{
+	public AudioMixer	mixer;
+	[Range(0.0001f, 1f)]
+	public float		volume = 0.5f;
+
+	public void SetVolume(float newVolume)
+	{
+		mixer.SetFloat("MusicVolume", Mathf.Log10(newVolume) * 20);
+	}
+}
