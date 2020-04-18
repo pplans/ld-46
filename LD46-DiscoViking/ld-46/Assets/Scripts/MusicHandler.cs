6e8bc70c5261a29001598e334ebc92e8f65e1139
Otakode <marchal.cl@gmail.com>
2020-04-18 14:51:50 +0200

    bpm music
    Signed-off-by: Otakode <marchal.cl@gmail.com>

---
+++ LD46-DiscoViking/ld-46/Assets/Scripts/MusicHandler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class MusicHandler : MonoBehaviour
+{
+	public AudioSource music;
+	public InputAction action;
+	public CanvasGroup group;
+	public AnimationCurve curve;
+
+	public float bpm;
+	public float bpmOffset;
+
+	public float inputValidity;
+
+	private void Start()
+	{
+		action.performed += InputLink;
+		action.Enable();
+		music.Play();
+		bpmOffset += Time.time;
+	}
+	private void InputLink(InputAction.CallbackContext callbackContext)
+	{
+		Debug.Log(ValidateBeat());
+	}
+
+	private void Update()
+	{
+		group.alpha = curve.Evaluate( 1f - ( Mathf.Abs( GetBeatOffset() ) * 4f ) );
+	}
+
+	private float GetBeatOffset()
+	{
+		float timeValue = Time.time - bpmOffset;
+
+		timeValue *= bpm / 60;
+
+		return timeValue - Mathf.Round(timeValue);
+	}
+
+	public bool ValidateBeat()
+	{
+		return Mathf.Abs(GetBeatOffset()) <= inputValidity;
+	}
+}
