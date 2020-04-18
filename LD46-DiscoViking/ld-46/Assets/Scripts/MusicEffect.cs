a81e5f32a0424a0db152eaa035e0a01520ad69e9
Otakode <marchal.cl@gmail.com>
2020-04-18 21:13:06 +0200

    beeps
    Signed-off-by: Otakode <marchal.cl@gmail.com>

---
+++ LD46-DiscoViking/ld-46/Assets/Scripts/MusicEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicEffect : MonoBehaviour
+{
+	public AudioSource music;
+	public AudioSource beep;
+	public MusicHandler musicHandler;
+
+	public float ratio = 1f;
+
+	private void Start()
+	{
+		musicHandler.OnBeat.AddListener(PlayBeep);
+	}
+
+	private void Update()
+	{
+		music.volume = ratio;
+		beep.volume = 1f - ratio;
+	}
+
+	private void PlayBeep()
+	{
+		beep.Stop();
+		beep.Play();
+	}
+}
