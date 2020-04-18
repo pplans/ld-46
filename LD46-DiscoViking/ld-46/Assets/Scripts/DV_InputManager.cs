4ff6b4e03fc2c742c563c98585af5c6c9507955c
BlinK3d-GD <BlinK3d-GD@users.noreply.github.com>
2020-04-18 19:31:12 +0200

    Centralized Level with Input validity relative to music beat + generation of grid via World prefab

---
+++ LD46-DiscoViking/ld-46/Assets/Scripts/DV_InputManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DV_InputManager : MonoBehaviour
+{
+    public InputAction action;
+    public DV_GameManager gameManager;
+    private bool bInputDetectionActive;
+
+    private void Start()
+    {
+        action.performed += InputLink;
+        action.Enable();
+
+        bInputDetectionActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bInputDetectionActive = gameManager.bGameStarted;
+
+    }
+
+    private void InputLink(InputAction.CallbackContext callbackContext)
+    {
+        if (bInputDetectionActive)
+        {
+            bool valid = gameManager.musicHandler.ValidateBeat();
+
+            if (valid)
+            {
+                Debug.Log(gameManager.musicHandler.GetBeatOffset());
+            }
+            
+        }        
+
+    }
+}
