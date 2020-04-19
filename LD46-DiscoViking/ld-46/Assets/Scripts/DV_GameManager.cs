70e79be6018051f48beadbc27adf7afc6c3170bb
Otakode <marchal.cl@gmail.com>
2020-04-19 20:46:13 +0200

    DV_GameManager init on awake

--- LD46-DiscoViking/ld-46/Assets/Scripts/DV_GameManager.cs
+++ LD46-DiscoViking/ld-46/Assets/Scripts/DV_GameManager.cs
@@ -21,7 +21,7 @@ public class DV_GameManager : MonoBehaviour
     public string currentGamePhase;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         bGameStarted = false;
         bBeatValidated = false;
