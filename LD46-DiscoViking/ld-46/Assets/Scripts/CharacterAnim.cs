1d360df8c1759f5a012d2e223acfac11c0588088
BlinK3d-GD <BlinK3d-GD@users.noreply.github.com>
2020-04-19 18:15:28 +0200

    Animated player character

---
+++ LD46-DiscoViking/ld-46/Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Animator>().Play("DiscoUp");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnimationStep()
+    {
+        GetComponent<Animator>().SetTrigger("Step");
+    }
+}
