e5e196c1eab994019158b8800a36f5b950278c3e
Otakode <marchal.cl@gmail.com>
2020-04-19 12:53:42 +0200

    discoball Fail feedback

--- LD46-DiscoViking/ld-46/Assets/Scripts/UI/DiscoBall.cs
+++ LD46-DiscoViking/ld-46/Assets/Scripts/UI/DiscoBall.cs
@@ -13,6 +13,8 @@ public class DiscoBall : MonoBehaviour
     private int maxDisco;
     private int currDisco;
 
+    private float failTime = -5f;
+
     void Awake()
     {
         maxDisco = 100;
@@ -22,6 +24,18 @@ public class DiscoBall : MonoBehaviour
     void Update()
     {
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.Self);
+
+        float failRatio = failTime - Time.time;
+        if (failRatio > 0f)
+            transform.localPosition = new Vector3(Random.Range(-failRatio * 0.5f, failRatio * 0.5f), Random.Range(-failRatio * 0.3f, failRatio * 0.3f), 0f);
+        else
+            transform.localPosition = Vector3.zero;
+
+    }
+
+    public void FailAnimation()
+    {
+        failTime = Time.time + 0.5f;
     }
 
     public void SetRotationSpeed(float speed)
